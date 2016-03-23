85a5bfbf04bb91510a6bcaee61cf24a8c297db40
lifan <lifan121@gmail.com>
2016-03-23 13:38:14 +0800

    Update solution.c

--- 101-Symmetric-Tree/solution.c
+++ 101-Symmetric-Tree/solution.c
@@ -1,46 +1,17 @@
-import Queue
+bool isMirror(struct TreeNode *tree1, struct TreeNode *tree2){
+    if (!tree1 && !tree2){
+        return true;
+    }
+    else if (tree1 && tree2){
+        return (tree1->val == tree2->val && 
+        isMirror(tree1->left, tree2->right) && 
+        isMirror(tree1->right, tree2->left));
+    }
+    else{
+        return false;
+    }
+}
 
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def isSymmetric(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if not root:
-            return True
-        list = self.inorderTraversal(root)
-        if len(list)%2 == 0:
-            return False
-        i, j = 0, len(list)-1
-        while i<j:
-            if (list[i] != list[j]):
-                return False
-            i += 1
-            j -= 1
-        return True
-
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        stack = Queue.LifoQueue()
-        ret_list = []
-        while root or not stack.empty():
-            while root:
-                stack.put(root)
-                root = root.left
-            if not stack.empty():
-                node = stack.get()
-                ret_list.append(node.val)
-                root = node.right
-        return ret_list
-
-if __name__ == '__main__':
-    pass
+bool isSymmetric(struct TreeNode* root) {
+    return isMirror(root, root);
+}
