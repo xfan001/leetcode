76a8d69cf994538d24df40b41e8b96d4ffaabc5c
lifan <lifan121@gmail.com>
2016-03-23 15:05:19 +0800

    Update solution.c

--- 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.c
+++ 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.c
@@ -1,15 +1,30 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* buildTreeFunc(int *inorder, int in_start, int in_end,
+                               int *postorder, int post_start, int post_end){
+    if (in_start > in_end){
+        return NULL;
+    }
+    int i;
+    for (i=in_start; i<=in_end; i++){
+        if (inorder[i] == postorder[post_end]){
+            break;
+        }
+    }
+    struct TreeNode *root = malloc(sizeof(struct TreeNode));
+    root->val = inorder[i];
+    root->left = buildTreeFunc(inorder, in_start, i-1, postorder, post_start, post_start+i-1-in_start);
+    root->right = buildTreeFunc(inorder, i+1, in_end, postorder, post_end-1-(in_end-i-1), post_end-1);
+    return root;
+}
 
-class Solution(object):
-    def buildTree(self, inorder, postorder):
-        """
-        :type inorder: List[int]
-        :type postorder: List[int]
-        :rtype: TreeNode
-        """
-        
+
+struct TreeNode* buildTree(int* inorder, int inorderSize, int* postorder, int postorderSize) {
+    return buildTreeFunc(inorder, 0, inorderSize-1, postorder, 0, postorderSize-1);
+}
